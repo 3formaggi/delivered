82c8cc7d6d235eda218a74ee8bd20e1702b412bd
Georg Delp <georg@georgdelp.de>
2020-03-22 19:14:52 +0100

    cloud Functions improved

--- functions/index.js
+++ functions/index.js
@@ -80,6 +80,10 @@ exports.completeProfile = functions.https.onCall((data, context) => {
                     return;
                 }
 
+                if(i !== "birth" && i !== "zip") {
+                    data[i] = data[i].trim();
+                }
+
 
             }
 
@@ -115,12 +119,18 @@ exports.completeProfile = functions.https.onCall((data, context) => {
     });
 });
 
+
 exports.telephonerGetTasks = functions.https.onCall((data, context) => {
     return new Promise(async function (resolve) {
         try {
 
             if(!data.fname || !data.lname || !data.zip){
                 resolve({state:"error",error:"params not complete"});
+                return;
+            }
+
+            if(typeof data.fname !== "string" || typeof data.lname !== "string" || typeof data.zip !== "number"){
+                resolve({state:"error",error:"param has wrong type"});
                 return;
             }
 
@@ -136,7 +146,7 @@ exports.telephonerGetTasks = functions.https.onCall((data, context) => {
                 return;
             }
             if (telephonerDoc.data().type !== "telephoner") {
-                resolve({state: "error", error: "shopper"});
+                resolve({state: "error", error: "type"});
             } else {
                 let tasksSnapshot = await admin.firestore().collection('tasks').where("fname","==",data.fname).where("lname","==",data.lname).where("zip", "==", data.zip).orderBy("delivered").orderBy('date', "desc").get();
                 if (tasksSnapshot.empty) {
@@ -166,4 +176,129 @@ exports.telephonerGetTasks = functions.https.onCall((data, context) => {
 
     });
 });
+
+exports.telephonerAddTask = functions.https.onCall((data, context) => {
+    return new Promise(async function (resolve) {
+        try {
+
+            if (!context.auth.uid) {
+                resolve({state: "error", error: 'not authenticated'});
+                return;
+            }
+
+            if(typeof data.birth !== "number" || data.birth < 0 || data.birth > Math.round(new Date().getTime() / 1000) ||
+                typeof data.country !== "string" || data.country.length !== 2 ||
+                typeof data.fname !== "string" || data.fname.length > 100 ||
+                typeof data.lname !== "string" || data.lname.length > 100 ||
+                typeof data.number !== "string" || data.number.length > 5 ||
+                typeof data.over16 !== "boolean" ||
+                typeof data.over18 !== "boolean" ||
+                typeof data.phone !== "number" || data.phone.length > 50 ||
+                typeof data.street !== "string" || data.street.length > 100 ||
+                typeof data.zip !== "number" || data.zip.length !== 5){
+                resolve({state:"error",error:"params not complete"});
+                return;
+            }
+
+            if(data.addressInfo && typeof data.addressInfo !== "string"){
+                resolve({state:"error",error:"param has wrong type"});
+                return;
+            }
+
+
+            if(typeof data.number !== "string" || data.number.toString().length > 5){
+                resolve('invalid number');
+                return;
+            }
+            if(typeof data.street !== "string" || data.street.length > 400){
+                resolve('invalid street');
+                return;
+            }
+
+
+            let telephonerDoc = await admin.firestore().collection('users').doc(context.auth.uid).get();
+            if (!telephonerDoc.exists) {
+                resolve({state: "error", error: "doc not found"});
+                return;
+            }
+            if (telephonerDoc.data().type !== "telephoner") {
+                resolve({state: "error", error: "type"});
+            } else {
+
+            }
+
+
+
+        }catch(error){
+            console.error(error);
+            resolve({state: "error", error: "internal"})
+        }
+    });
+});
+
+exports.userGetTasks = functions.https.onCall((data, context) => {
+    return new Promise(async function (resolve) {
+        try {
+
+            if (!context.auth.uid) {
+                resolve({state: "error", error: 'not authenticated'});
+                return;
+            }
+            
+            if(!data.zip || typeof data.over16 === "undefined" || typeof data.over18 === "undefined"){
+                resolve({state:"error",error:"request not complete"});
+                return;
+            }
+            
+            if(data.over18 && !data.over16){
+                resolve({state:"error",error:"over 16 can't be false when over 18 is true"});
+                return;
+            }
+            
+
+            let promises = [];
+            data.zip.forEach(function(element){
+                if(typeof element === "number" && element.toString().length === 5) {
+                    if(data.over18){
+                        promises.push(admin.firestore().collection('tasks').where('zip', '==', element).get());
+                    }else if(data.over16){
+                        promises.push(admin.firestore().collection('tasks').where('zip', '==', element).where('over18','==',false).get());
+                    }else{
+                        promises.push(admin.firestore().collection('tasks').where('zip', '==', element).where('over16','==',false).get());
+                    }
+                }else{
+                    resolve({state:"error",error:"zip invalid"});
+                }
+            });
+
+            if(promises === []){
+                return;
+            }
+
+            let result = [];
+            let temp = {};
+            Promise.all(promises)
+                .then(function(snapshots){
+                    snapshots.forEach(function(snapshot){
+                        if(!snapshot.empty){
+                            snapshot.forEach(function(doc){
+                                temp.street = doc.data().street;
+                                temp.zip = doc.data().zip;
+                            });
+                            result.push(temp);
+                        }
+
+                        temp = {};
+                    });
+                    console.log(result);
+                    resolve(result);
+                });
+
+        } catch (error) {
+            console.error(error);
+            resolve({state:"error",error:"internal"});
+        }
+    });
+});
+
 
